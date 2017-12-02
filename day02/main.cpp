4304b1cdbd50db0b6295a96f533563fbe0220cae
Jan-Erik Rediger <janerik@fnordig.de>
2017-12-02 16:23:30 +0100

    day 2, part 2

--- day02/main.cpp
+++ day02/main.cpp
@@ -8,6 +8,8 @@
 int main(void)
 {
 	std::vector<int> csum_numbers;
+	std::vector<int> csum_numbers2;
+
 	for (std::string line; std::getline(std::cin, line);) {
 		std::stringstream inp(line);
 		std::vector<int> numbers;
@@ -21,10 +23,24 @@ int main(void)
 		int max = *std::max_element(numbers.begin(), numbers.end());
 
 		csum_numbers.push_back(max-min);
+
+		int len = numbers.size();
+		for(int i=0; i<len-1; i++) {
+			for (int j=i+1; j<len; j++) {
+				int a = std::max(numbers[i], numbers[j]);
+				int b = std::min(numbers[i], numbers[j]);
+				if (a%b == 0) {
+					csum_numbers2.push_back(a/b);
+				}
+			}
+		}
 	}
 
 	int csum = std::accumulate(csum_numbers.begin(), csum_numbers.end(), 0);
+	int csum2 = std::accumulate(csum_numbers2.begin(), csum_numbers2.end(), 0);
+
 	std::cout << csum << std::endl;
+	std::cout << csum2 << std::endl;
 
     return 0;
 }
