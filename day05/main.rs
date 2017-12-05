e7a79df4379b35c968108a471da4e92a8e894cc1
Jan-Erik Rediger <janerik@fnordig.de>
2017-12-05 15:04:39 +0100

    Day 5, part 2

--- day05/main.rs
+++ day05/main.rs
@@ -2,15 +2,18 @@ use std::io::prelude::*;
 use std::fs::File;
 use std::io::BufReader;
 
-fn jumps_to_outside(mut jumps: Vec<i32>) -> u32 {
+fn jumps_to_outside(mut jumps: Vec<i32>, part2: bool) -> u32 {
     let mut steps = 0;
     let len = jumps.len();
     let mut pos = 0i32;
 
     while pos >= 0 && pos < len as i32 {
         let nex = jumps[pos as usize];
-        jumps[pos as usize] += 1;
-
+        if part2 && nex >= 3 {
+            jumps[pos as usize] -= 1;
+        } else {
+            jumps[pos as usize] += 1;
+        }
         pos += nex;
         steps += 1;
     }
@@ -29,8 +32,10 @@ fn main() {
         .map(|l| l.unwrap().parse::<i32>().unwrap()).
         collect::<Vec<_>>();
 
-    let n = jumps_to_outside(jumps);
-    println!("Jumps: {}", n);
+    let n = jumps_to_outside(jumps.clone(), false);
+    let n2 = jumps_to_outside(jumps, true);
+    println!("Jumps, part 1: {}", n);
+    println!("Jumps, part 2: {}", n2);
 }
 
 #[cfg(test)]
@@ -39,6 +44,11 @@ mod test {
 
     #[test]
     fn sample() {
-        assert_eq!(5, jumps_to_outside(vec![0,3,0,1,-3]));
+        assert_eq!(5, jumps_to_outside(vec![0,3,0,1,-3], false));
+    }
+
+    #[test]
+    fn part2() {
+        assert_eq!(10, jumps_to_outside(vec![0,3,0,1,-3], true));
     }
 }
